56ffc87c1bc4a4060af94d62a0892baacf03659a
nireo <eemil.lehtikangas1@gmail.com>
2025-12-26 14:34:00 +0200

    updated install script and removed a useless dir

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cp nt.py ~/.local/bin/nt
-chmod +x ~/.local/bin/nt
+SOURCE_PATH="$(pwd)/nt.py"
+DEST_PATH="$HOME/.local/bin/nt"
+
+rm -f "$DEST_PATH"
+ln -s "$SOURCE_PATH" "$DEST_PATH"
+chmod +x "$SOURCE_PATH"
+
+echo "symlink created: $DEST_PATH -> $SOURCE_PATH"
